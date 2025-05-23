1f696ff616295ce89c92434a08e680eb6b6ee4ad
kashif019 <km.kashifkash@gmail.com>
2025-05-23 12:14:58 +0530

    navbar,story

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,11 +1,27 @@
-import React from 'react'
-// import './Style.css'
+import React from "react";
+import { FaSearch } from "react-icons/fa";
+import "./Style.css";
+import { FaFacebook } from "react-icons/fa";
+
 function Navbar() {
   return (
-    <div class="end">
-     
+    <div className="navbar-btn">
+      <div>
+        <button className="btn1">RACELINE</button>
+        <button className="btn2">EVENTS</button>
+        <button className="btn3">COMMUNITY</button>
+        <button className="btn4">GARAGE</button>
+        <button className="btn5">MEDIA</button>
+      </div>
+      <div>
+        <button className="btn6">
+          <FaSearch />
+        </button>
+        <button className="btn7">JOIN NOW</button>
+       
+      </div>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
